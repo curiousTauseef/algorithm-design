3ac699ddc426dcf632417bcb5abf3bc68dd31060
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2014-10-23 21:25:27 +0200

    Adds functions sort_and_count and merge_and_count_split

---
+++ inversions.py
@@ -0,0 +1,34 @@
+def sort_and_count(A, n):
+  if n == 1:
+    return (A, 0)
+  else:
+    middle = n//2
+    (left, count_left) = sort_and_count(A[:middle], middle)
+    (right, count_right) = sort_and_count(A[middle:], n-middle)
+    (merged, count_split) = merge_and_count_split(left, right)
+    return (merged, count_left+count_right+count_split)
+
+def merge_and_count_split(left, right):
+  count = 0
+  merged = []
+  while left or right:
+    if left and right:
+      if left[0] <= right[0]:
+        merged.append(left[0])
+        left = left [1:]
+      else:
+        merged.append(right[0])
+        right = right[1:]
+        count += len(left)
+    elif left:
+      merged.append(left[0])
+      left = left[1:]
+    elif right:
+      merged.append(right[0])
+      right = right[1:]
+  return (merged, count)
+
+
+a = [1,2,3,4,5,7,6,9,8,10]
+count=sort_and_count(a, len(a))
+print (count)
