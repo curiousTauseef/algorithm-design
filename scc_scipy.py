de39ce974839b12e3b1e13191311fcb29ac795c8
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2014-11-18 16:36:55 +0100

    Renames some variables to improve readability

--- scc_scipy.py
+++ scc_scipy.py
@@ -1,10 +1,10 @@
 from scipy.sparse import coo_matrix, csgraph
 import numpy as np
 
-def compute_scc(file, number_of_nodes):
+def compute_scc(file, n):
     with open(file) as f:
-        data = np.array([list(map(int, line.split())) for line in f])
-        adj_matrix = coo_matrix((np.ones(len(data)), (data[:, 0], data[:, 1])), shape = (number_of_nodes, number_of_nodes))
+        graph = np.array([list(map(int, line.split())) for line in f])
+        xs, ys = graph[:, 0], graph[:, 1]
+        data = np.ones(len(graph))
+        adj_matrix = coo_matrix((data, (xs, ys)), shape = (n, n))
         return csgraph.connected_components(adj_matrix, connection = 'strong')
-
-compute_scc('graf', 875715)
