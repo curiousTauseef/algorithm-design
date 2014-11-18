babdedbea2158f8b39bd115de015e96aa98ba4a7
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2014-11-18 14:31:32 +0100

    Tests np.loadtxt to load graph from file

--- scc_scipy.py
+++ scc_scipy.py
@@ -1,18 +1,10 @@
-from scipy import sparse
+# from scipy.sparse import coo_matrix as matrix
+# from scipy.sparse.csgraph import connected_components
 import numpy as np
 
-number_of_nodes = 875714
+def compute_scc(file, number_of_nodes):
+    x, y = np.loadtxt(file, 'int', unpack = True)
+    # adj_matrix = sparse.coo_matrix((np.ones(len(i_indices)), (i_indices, j_indices)), shape = (875715, 875715))
+    # return connected_components(adj_matrix, connection = 'strong')
 
-i_indices, j_indices = [], []
-f = open('graf')
-for line in f:
-    i_index, j_index = line.split()
-    i_indices.append(int(i_index))
-    j_indices.append(int(j_index))
-f.close()
-
-adj_matrix = sparse.coo_matrix((np.ones(len(i_indices)), (i_indices, j_indices)), shape = (875715, 875715))
-connected_components = sparse.csgraph.connected_components(adj_matrix, connection = 'strong')
-components = sorted(np.bincount(connected_components[1]), reverse = True)
-for i in range(5):
-    print(components[i])
+compute_scc('graf', 2)
